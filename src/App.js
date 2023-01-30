d026f6b1ad4e38fe2d0a3593048234e1e69edde6
daniel-liberto <daniel.liberto@hotmail.com>
2023-01-30 18:35:29 -0300

    normalize.css removed from the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Teste</h1>
+      <h2>Teste</h2>
+      <h3>Teste</h3>
+      <p>teste É um teste</p>
+      <ul>
+        <li>
+          <a href="/">Vamo testar</a>
+        </li>
+        <li>
+          <a href="/">Vamo testar</a>
+        </li>
+        <li>
+          <a href="/">Vamo testar</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
