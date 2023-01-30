ef9ac2a841db3bdc1a6d1c4858acbe2a6cd913df
daniel-liberto <daniel.liberto@hotmail.com>
2023-01-30 18:52:07 -0300

    reset css manually

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div>
-      <h1>Teste</h1>
-      <h2>Teste</h2>
-      <h3>Teste</h3>
-      <p>teste É um teste</p>
-      <ul>
-        <li>
-          <a href="/">Vamo testar</a>
-        </li>
-        <li>
-          <a href="/">Vamo testar</a>
-        </li>
-        <li>
-          <a href="/">Vamo testar</a>
-        </li>
-      </ul>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
