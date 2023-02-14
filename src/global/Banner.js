681728277c9b976d37fe892afb2d5225f4211841
daniel-liberto <daniel.liberto@hotmail.com>
2023-02-14 19:42:33 -0300

    banner component finished and add active tabs style

--- src/global/Banner.js
+++ src/global/Banner.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import HeaderTitle from './HeaderTitle';
+import './Banner.css';
 
-const Banner = ({ image, text, hx, textAlign, margin, padding, fontSize }) => {
+const Banner = ({
+  image,
+  text,
+  hx,
+  textAlign,
+  margin,
+  padding,
+  fontSize,
+  color,
+  content,
+}) => {
   return (
-    <div>
-      <img src={`./img/banners/${image}`} alt="" />
-      <HeaderTitle
-        text={text}
-        hx={hx}
-        textAlign={textAlign}
-        margin={margin}
-        padding={padding}
-        fontSize={fontSize}
-      />
+    <div className="bg-banner">
+      <div className="banner">
+        <img
+          src={image}
+          alt=""
+          style={{ margin: '0 auto', gridRow: '1', gridColumn: '1' }}
+        />
+        <div className="banner-title">
+          <HeaderTitle
+            text={text}
+            hx={hx}
+            textAlign={textAlign}
+            margin={margin}
+            padding={padding}
+            fontSize={fontSize}
+            color={color}
+            content={content}
+          />
+        </div>
+      </div>
     </div>
   );
 };
--- src/info/Info.js
+++ src/info/Info.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import Banner from '../global/Banner';
 import './Info.css';
+import bgInfo from '../img/banners/bg-info.jpg';
 
 const Info = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Banner
+        image={bgInfo}
+        hx="h1"
+        fontSize="60"
+        color="#ffffff"
+        content=""
+        text="Informações"
+      />
+    </div>
+  );
 };
 
 export default Info;
