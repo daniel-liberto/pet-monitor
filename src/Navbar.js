fb0fd58ae14395e450fe9bdfdea7ee9e0ceae4cf
daniel-liberto <daniel.liberto@hotmail.com>
2023-03-01 16:41:40 -0300

    mobile layout started and hamburger menu created

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './Navbar.css';
+import './mobile/NavbarMobile.css';
 import logoPetMonitor from './img/svg/logo-petmonitor.svg';
 import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+  const [active, setActive] = React.useState(false);
+  const ToggleMode = () => {
+    setActive(!active);
+  };
+
   return (
     <div className="bg-navbar">
       <div className="container-navbar">
@@ -12,23 +18,38 @@ const Navbar = () => {
             <img src={logoPetMonitor} alt="Logomarca da empresa Pet Monitor" />
           </a>
         </div>
-        <ul className="navbar-menu">
-          <li>
-            <NavLink to="info">Info</NavLink>
-          </li>
-          <li>
-            <NavLink to="monitoramento">Monitoramento</NavLink>
-          </li>
-          <li>
-            <NavLink to="sobre">Sobre</NavLink>
-          </li>
-          <li>
-            <NavLink to="contato">Contato</NavLink>
-          </li>
-          <li>
-            <NavLink to="login">Login</NavLink>
-          </li>
-        </ul>
+
+        <div className="hamburguer-container">
+          <div
+            className={active ? 'icon iconActive' : 'icon'}
+            onClick={ToggleMode}
+          >
+            <div className="hamburguer hambuguerIcon"></div>
+          </div>
+          <ul
+            className={
+              active
+                ? 'menu navbar-menu menuOpen'
+                : 'menu navbar-menu menuClose'
+            }
+          >
+            <li>
+              <NavLink to="info">Info</NavLink>
+            </li>
+            <li>
+              <NavLink to="monitoramento">Monitoramento</NavLink>
+            </li>
+            <li>
+              <NavLink to="sobre">Sobre</NavLink>
+            </li>
+            <li>
+              <NavLink to="contato">Contato</NavLink>
+            </li>
+            <li>
+              <NavLink to="login">Login</NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
