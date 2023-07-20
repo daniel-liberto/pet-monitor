04889470555616618794dd4ba6eaad4313b80f66
daniel-liberto <daniel.liberto@hotmail.com>
2023-07-20 18:35:51 -0300

    starting model api and adding flask sqlalchemy pip

--- back-end/server.py
+++ back-end/server.py
@@ -1,24 +1,18 @@
 # Import flask and datetime module for showing date and time
-from flask import Flask
-import datetime
-
-x = datetime.datetime.now()
+from flask import Flask, jsonify, request
 
 # Initializing flask app
 app = Flask(__name__)
 
-
 # Route for seeing a data
-@app.route('/data')
-def get_time():
 
-    # Returning an api for showing in reactjs
-    return {
-        'Name': "geek",
-        "Age": "22",
-        "Date": x,
-        "programming": "python"
-    }
+
+@app.route('/useradd', methods=['POST'])
+def useradd():
+    name = request.json['name']
+    email = request.json['email']
+
+    return jsonify({'success': 'Success post'})
 
 
 # Running app
