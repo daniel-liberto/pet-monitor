a4587347f1a4590b544349a3f252aa24140eed21
daniel-liberto <daniel.liberto@hotmail.com>
2023-07-21 12:32:08 -0300

    some minor banner changes

--- back-end/server.py
+++ back-end/server.py
@@ -1,12 +1,17 @@
-# Import flask and datetime module for showing date and time
+# Import flask and sqlalchemy pip
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
 # Initializing flask app
 app = Flask(__name__)
 
-# Route for seeing a data
+# Database configuration                        Username:password@hostname/databasename
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:''@localhost/petmonitor'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 
+# Route for add user
 @app.route('/useradd', methods=['POST'])
 def useradd():
     name = request.json['name']
